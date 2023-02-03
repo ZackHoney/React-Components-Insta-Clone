2ba7e027286db1686bdde69693f8083091411eda
Zachary Honey <zhoney466@gmail.com>
2023-02-02 22:24:14 -0800

    Completed MVP items

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from "./components/Posts/Posts"
+import SearchBar from "./components/SearchBar/SearchBar"
 // Import the dummyData
+import dummyData from './dummy-data'
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
+
 
   const likePost = postId => {
     /*
@@ -27,11 +32,28 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-  };
+    const updatedPosts = posts.map(post => {
+      if (post.id === postId) {
+        return {...post, likes: post.likes + 1};
+      }else {
+        return post;
+      }
+    })
 
+    setPosts(updatedPosts);
+  };
+ /**
+  * Higher order functions
+  * map -> loop over an array, do something to each element, and return a new array
+  * filter -> loop over an array, based on a condition, return particular elements into a new array
+  * reduce -> loop over an array and reduce it to a singular value
+  * find -> loop over an array, based on a condition, return the FIRST element it finds that matches that condition
+  */
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts posts={posts} likePost={likePost} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
